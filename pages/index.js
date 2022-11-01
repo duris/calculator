35f806040723f0e61351cc096848d700888f5d4f
Ross <ross@duris.io>
2022-11-01 19:28:03 -0400

    first commit

---
+++ pages/index.js
@@ -0,0 +1,38 @@
+import { useState } from "react"
+
+const Home = () => {
+
+  const [calcNums, setCalcNums] = useState([0,1,2,3,4,5,6,7,8,9,'+','-','*','/','='])
+
+  const [head, setHead] = useState(0)
+
+  const increaseHead = () => {
+    setHead(head+10)
+  }
+  const decreaseHead = () => {
+    setHead(head-10)
+  }
+  const setCalc = (num) => {
+    setHead(`${head}` + `${num}`)
+  }
+  return(
+    <div className=" bg-slate-900 h-screen">
+      <div>
+        
+      <div className=" text-5xl w-full inline-block text-center p-2">{head}</div>
+        <div className="grid grid-cols-3 max-w-[300px] m-auto text-center">
+          
+        {/* <div>
+          <button onClick={increaseHead}>+</button>
+          <button onClick={decreaseHead}>-</button>
+        </div> */}
+        {calcNums.map((num) => 
+          <div className="hover:bg-slate-700 cursor-pointer transition-all duration-300 py-4 m-1 rounded-md bg-gray-800" onClick={() => setHead(num)}>{num}</div>
+        )}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
